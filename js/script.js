8293108c18ca44dfa05723863f2bb5ec22b5ed70
Mindy Tomlinson <mindy.tomlinson@gmail.com>
2015-04-07 21:23:36 -0400

    Add footer

--- js/script.js
+++ js/script.js
@@ -27,23 +27,9 @@ function calculate() {
   // and we have meaningful results to display
   if (isFinite(monthly)) {
     // Fill in the output fields, rounding to 2 decimal places
-    payment.innerHTML = formatNumber(monthly);
-    total.innerHTML = formatNumber(monthly * payments);
-    totalinterest.innerHTML = formatNumber((monthly * payments) - principal);
-    
-    // Save the user's input so we can restore it
-    // the next time they visit
-    save(amount.value, apr.value, years.value, zipcode.value);
-    
-    // Advertise: find and display local lenders
-    // but ignore network errors
-    try { // Catch any errors that occur within these curly braces...
-
-    }
-    
-    catch(e) {
-      // ... And ignore these errors
-    }
+    payment.innerHTML = formatNumber(monthly, 2);
+    total.innerHTML = formatNumber(monthly * payments, 2);
+    totalinterest.innerHTML = formatNumber((monthly * payments) - principal, 2);
     
     // Finally, chart loan balance, interest, and equity payments
     chart(principal, rate, monthly, payments)
@@ -59,41 +45,13 @@ function calculate() {
   }
 }
 
-// Save the user's input as properties of the localStorage object.
-// Those properties will still be there
-// when the user visits in the future.
-// This storage feature will not work in some browsers
-// (eg, Firefox) if you run the example from a local file URL
-// It does work over HTTP, however.
-function save(amount, apr, years, zipcode) {
-  if (window.localStorage) { // Only do this if the browser supports it
-    localStorage.loan_amount = amount;
-    localStorage.loan_apr = apr;
-    localStorage.loan_years = years;
-    localStorage.loan_zipcode = zipcode;
-  }
-}
-
-// Automatically attempt to restore input fields
-// when the document first loads.
-window.onload = function() {
-  // If the browser supports localStorage
-  // and we have some stored data
-  if (window.localStorage && localStorage.loan_amount) {
-    document.getElementById("amount").value = localStorage.loan_amount;
-    document.getElementById("apr").value = localStorage.loan_apr;
-    document.getElementById("years").value = localStorage.loan_years;
-    document.getElementById("zipcode").value = localStorage.loan_zipcode;
-  }
-};
-
 // Chart the monthly loan balance, interest, and
 // equity in an HTML <canvas> element.
 // If called with no arguments, then just erase
 // any previously drawn chart.
 function chart(principal, rate, monthly, payments) {
   var graph = document.getElementById("graph");
-  graph.width = graph.width; // Magic to clear and reset the canvas element
+  graph.width = graph.width;      // Magic to clear and reset the canvas element
   
   // If we're called with no arguments, 
   // or if the browser does not support graphics
@@ -102,8 +60,10 @@ function chart(principal, rate, monthly, payments) {
   
   // Get the "context" object for the <canvas>
   // that defines the API
-  var g = graph.getContext("2d"); // All drawing is done with this object
-  var width = graph.width, height = graph.height; // Get canvas size
+  // All drawing is done with this object
+  var g = graph.getContext("2d");
+  
+  var width = graph.width, height = graph.height;   // Get canvas size
   
   // These functions convert payment numbers
   // and dollar amounts to pixels
@@ -116,13 +76,69 @@ function chart(principal, rate, monthly, payments) {
   
   // Payments are a straight line from (0,0)
   // to (payments, monthly * payments)
-  g.moveTo(paymentToX(0), amountToY(0)); // Start at lower left
-  g.lineTo(paymentToX(payments), // Draw to upper right
+  g.moveTo(paymentToX(0), amountToY(0));            // Start at lower left
+  g.lineTo(paymentToX(payments),                    // Draw to upper right
            amountToY(monthly * payments));
-  g.lineTo(paymentToX(payments), amountToY(0))
+  g.lineTo(paymentToX(payments), amountToY(0));     // Down to lower right
+  g.closePath();                                    // And back to start
+  g.fillStyle = "#72AAD0";                          // Light blue
+  g.fill();                                         // Fill the triangle
+  
+  // Cumulative equity is non-linear and trickier to chart
+  var equity = 0;
+  g.beginPath();
+  g.moveTo(paymentToX(0), amountToY(0));            // Start at lower left
+  for (var p = 1; p <= payments; p++) {
+    // For each payment, figure out how much is interest
+    var thisMonthsInterest = (principal - equity) * rate;
+    equity += (monthly - thisMonthsInterest);       // The rest goes to equity
+    g.lineTo(paymentToX(p), amountToY(equity));     // Line to this point
+  }
+  g.lineTo(paymentToX(payments), amountToY(0));     // Line back to X axis
+  g.closePath();  // And back to starting point
+  g.fillStyle = "#40FD65";
+  g.fill(); // Fill area under curve
+  
+  // Loop again, as above, but chart loan balance as a black line
+  var bal = principal;
+  g.beginPath();
+  g.moveTo(paymentToX(0), amountToY(bal));
+  for (var p = 1; p <= payments; p++) {
+    var thisMonthsInterest = bal*rate;
+    bal -= (monthly - thisMonthsInterest);          // The rest goes to equity
+    g.lineTo(paymentToX(p), amountToY(bal));        // Draw line to this point
+  }
+  
+  g.lineWidth = 2;
+  g.strokeStyle = "#444"
+  g.stroke();   // Draw the balance curve
+  
+  // Now make yearly tick marks and year numbers on X axis
+  g.textAlign = "center";                           // Center text over ticks
+  g.fillStyle = "#444";
+  g.font = "0.6em 'Open Sans'";
+  var y = amountToY(0);                             // Y coordinate of X axis
+  for (var year=1; year*12 <= payments; year++) {   // For each year
+    var x = paymentToX(year * 12);                  // Compute tick position
+    g.fillRect(x - 0.5, y - 3, 1, 3);               // Draw the tick
+    if (year === 1) g.fillText("Year", x, y - 5);   // Label the axis
+    if (year % 5 === 0 && year * 12 !== payments)   // Number every 5 years
+      g.fillText(String(year), x, y - 5);
+  }
+  
+  // Mark payment amounts along the right edge
+  g.textAlign = "right";                            // Right-justify the text
+  g.textBaseline = "middle"                         // Center it vertically
+  var ticks = [monthly * payments, principal];      // The two points we'll mark
+  var rightEdge = paymentToX(payments);             // X coordinate of Y axis
+  for (var i=0; i < ticks.length; i++) {            // For each of the 2 points
+    var y = amountToY(ticks[i]);                    // Compute y position of tick
+    g.fillRect(rightEdge - 3, y - 0.5, 3, 1);       // Draw the tick mark
+    g.fillText(String(formatNumber(ticks[i]), 2), rightEdge - 5, y);    // And label it
+  }
 }
 
-function formatNumber (num) {
-    return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+function formatNumber (num, decimals) {
+    return num.toFixed(decimals).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
     // http://blog.tompawlak.org/number-currency-formatting-javascript
 }
